e1cfa6955720d63f9680ec3b1d7510d8f01a89ee
Ragularavinth <ragul.aravinth17@gmail.com>
2024-11-06 14:05:07 +0530

    Delete and Update will be updated

--- server.js
+++ server.js
@@ -12,6 +12,13 @@ app.use(cors()); // Enable CORS for all routes
 
 app.use(express.urlencoded({ extended: true }));
 
+
+// app.use(cors({
+//   origin: 'https://mongo-cred.vercel.app/', // Replace with her actual frontend URL
+//   methods: ['GET', 'POST', 'PUT', 'DELETE'],
+//   allowedHeaders: ['Content-Type', 'Authorization']
+// }));
+
 // MongoDB connection
 const uri = process.env.MONGODB_URI;
 
@@ -33,6 +40,7 @@ const postSchema = new mongoose.Schema({
 
 const Post = mongoose.model("Post", postSchema);
 
+//Post the new data to api
 app.post("/api/posts", async (req, res) => {
   const newPost = new Post({
     course: req.body.course,
@@ -47,15 +55,62 @@ app.post("/api/posts", async (req, res) => {
   }
 });
 
+//get the all data from api
 app.get("/api/posts", async (req, res) => {
   try {
-    const limit = Number(req.query.limit);
+    const limit = Number(req.query.limit); // to limit the number of posts to show
     const posts = limit ? await Post.find().limit(limit) : await Post.find();
     res.status(200).json(posts);
   } catch (error) {
     res.status(500).json({ message: "Error fetching posts", error });
   }
 });
+
+// get the single data from api using id
+app.get("/api/posts/:id", async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    
+    if(post){
+      res.status(200).json(post);
+    }else{
+      res.status(404).json({ message: `Post with ${id} id not found` });
+    }
+  } catch (error) {
+    res.status(500).json({ message: "Error fetching post", error });
+  }
+});
+
+// update the single data from api using id
+app.put("/api/posts/:id", async (req, res) => {
+  try {
+    const Updatedpost = await Post.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    });
+    if(Updatedpost){
+      res.status(200).json(Updatedpost);
+    }else{
+      res.status(404).json({ message: `Post with ${req.params.id} id not found` });
+    }
+  }catch (error) {
+    res.status(500).json({ message: "Error updating post", error });
+  }
+});
+
+// delete the single data from api using id
+app.delete("/api/posts/:id", async (req, res) => {
+  try {
+    const deletedPost = await Post.findByIdAndDelete(req.params.id);
+    if(deletedPost){
+      res.status(200).json({ message: `Post with ${req.params.id} id deleted` });
+    }else{
+      res.status(404).json({ message: `Post with ${req.params.id} id not found` });
+    }
+  } catch (error) {
+    res.status(500).json({ message: "Error deleting post", error });
+  }
+});
+
 
 // Start the server
 const PORT = process.env.PORT || 3000;
